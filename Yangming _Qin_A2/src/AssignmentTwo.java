e37bbba6270893a7b05ef7ec885eaac5ccc1c975
452154 <146407557+452154@users.noreply.github.com>
2024-12-04 18:49:25 +0800

    part 3

--- Yangming _Qin_A2/src/AssignmentTwo.java
+++ Yangming _Qin_A2/src/AssignmentTwo.java
@@ -1,7 +1,26 @@
 public class AssignmentTwo {
     public static void main(String[] args) {
+        AssignmentTwo assignmentTwo = new AssignmentTwo();
+        System.out.println("Part Three-------------------");
+        assignmentTwo.partThree();
+
     }
     public void partThree(){
+        Ride ride = new Ride("Roller Coaster", 10.0, 4,
+                new Employee("Mary","1001",30,"operator","10"));
+
+        //Add visitors to the queue
+        ride.addVisitorToQueue(new Visitor("Devin","3",8,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Yang","4",21,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Ming","5",18,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Alice","1",15,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Bob","2",10,true,"Roller Coaster"));
+
+        //Remove a visitor
+        ride.removeFromQueue();
+
+        //Print detailed information of visitors in the queue
+        ride.printQueue();
     }
     public void partFourA(){
     }
--- Yangming _Qin_A2/src/Ride.java
+++ Yangming _Qin_A2/src/Ride.java
@@ -1,7 +1,11 @@
-public class Ride  {
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Ride implements RideInterface  {
     private String rideFacilityName;       // The name of the amusement equipment used for riding.
     private double price;              // The cost of a tourist riding on amusement facilities.
     private Employee rideOperator;   //operator
+    private Queue<Visitor> waitingQueue = new LinkedList<>();// Add visitors to the queue.
     public Ride(){}
     public Ride(String rideFacilityName, double price, int maxRider, Employee rideOperator){
         this.rideFacilityName = rideFacilityName;
@@ -17,4 +21,59 @@ public class Ride  {
     public void setRideOperator(Employee rideOperator){this.rideOperator = rideOperator;}
 
 
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        waitingQueue.add(visitor);
+        visitor.printDetails();
+        System.out.println("Added to the queue.");
+
+    }
+
+    @Override
+    public void removeFromQueue() {
+        if (!waitingQueue.isEmpty()) {
+            Visitor visitor = waitingQueue.poll();
+            visitor.printDetails();
+            System.out.println("Removed from the queue.");
+
+        } else {
+            System.out.println("The queue is empty and cannot be removed.");
+        }
+
+    }
+
+    @Override
+    public void printQueue() {
+        System.out.println("Visitors in the current queue:");
+        for (Visitor visitor : waitingQueue) {
+            visitor.printDetails();
+            System.out.println();
+        }
+
+    }
+
+    @Override
+    public void runOneCycle() {
+
+    }
+
+    @Override
+    public void addVisitorToHistory(Visitor visitor) {
+
+    }
+
+    @Override
+    public boolean checkVisitorFromHistory(Visitor visitor) {
+        return false;
+    }
+
+    @Override
+    public int numberOfVisitor() {
+        return 0;
+    }
+
+    @Override
+    public void printRideHistory() {
+
+    }
 }
