eddf094fcdcdf73cd069b4ddf63e989abd7f3189
452154 <146407557+452154@users.noreply.github.com>
2024-12-05 13:45:15 +0800

    part 5

--- Yangming _Qin_A2/src/AssignmentTwo.java
+++ Yangming _Qin_A2/src/AssignmentTwo.java
@@ -12,6 +12,10 @@ public class AssignmentTwo {
         System.out.println("Part FourB-------------------");
         assignmentTwo.partFourB();
 
+        System.out.println();
+        System.out.println("Part Five--------------------");
+        assignmentTwo.partFive();
+
     }
     public void partThree(){
         Ride ride = new Ride("Roller Coaster", 10.0, 4,
@@ -78,7 +82,34 @@ public class AssignmentTwo {
 
     }
     public void partFive(){
+        Ride ride = new Ride("Roller Coaster", 10.0, 4,
+                new Employee("Mary","1001",30,"operator","10"));
+
+        //Add visitors to the queue
+        ride.addVisitorToQueue(new Visitor("Devin","3",8,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Yang","4",21,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Ming","5",18,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Alice","1",15,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Bob","2",10,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Yu","7",8,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Zi","6",21,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("An","8",18,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Ni","9",15,true,"Roller Coaster"));
+        ride.addVisitorToQueue(new Visitor("Hao","10",15,true,"Roller Coaster"));
+
+        // Print all visitors in the queue
+        ride.printQueue();
+
+        // Run one cycle
+        ride.runOneCycle();
+
+        // Print all visitors in the queue after one cycle of operation
+        ride.printQueue();
+
+        // Print all visitors in the collection
+        ride.printRideHistory();
     }
+
     public void partSix(){
     }
     public void partSeven(){
--- Yangming _Qin_A2/src/Ride.java
+++ Yangming _Qin_A2/src/Ride.java
@@ -4,13 +4,17 @@ public class Ride implements RideInterface  {
     private String rideFacilityName;       // The name of the amusement equipment used for riding.
     private double price;              // The cost of a tourist riding on amusement facilities.
     private Employee rideOperator;   //operator
+    private int maxRider;       //How many tourists can be accommodated in one cycle?
+    private int numOfCycles = 0;     //The number of times the amusement facilities have been operated.
     private Queue<Visitor> waitingQueue = new LinkedList<>();// Add visitors to the queue.
     public List<Visitor> rideHistory = new LinkedList<>(); // Collection for storing historical records.
     public Ride(){}
     public Ride(String rideFacilityName, double price, int maxRider, Employee rideOperator){
         this.rideFacilityName = rideFacilityName;
         this.price = price;
+        this.maxRider = maxRider;
         this.rideOperator = rideOperator;
+
 
     }
     public String getRideFacilityName(){return rideFacilityName;}
@@ -54,6 +58,38 @@ public class Ride implements RideInterface  {
 
     @Override
     public void runOneCycle() {
+        if (rideOperator == null) {
+            System.out.println("The amusement equipment cannot operate because there is no assigned operator.");
+            return;
+        }
+
+        if (waitingQueue.isEmpty()) {
+            System.out.println("The amusement facilities will not operate as there are no waiting tourists in the queue.");
+            return;
+        }
+
+        //Declare an array list for storing visitors who have taken rides on amusement facilities.
+        List<Visitor> ridersThisCycle = new ArrayList<>();
+
+        while (!waitingQueue.isEmpty() && ridersThisCycle.size() < maxRider) {
+            Visitor visitor = waitingQueue.poll();
+            ridersThisCycle.add(visitor);
+        }
+
+        // Output the information of tourists who ride on the amusement facilities this time.
+        System.out.println("Passengers taking the amusement ride this time:");
+        for (Visitor visitor : ridersThisCycle) {
+            visitor.printDetails();
+            System.out.println("Already boarded");
+        }
+
+        // Add passenger to history
+        rideHistory.addAll(ridersThisCycle);
+
+        // Increase the frequency of operation of amusement facilities
+        numOfCycles++;
+        System.out.println("The amusement facility has run " + numOfCycles + " times.");
+
 
     }
 
